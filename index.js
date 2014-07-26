67bf9d7c1a34839ed5ceed46bb90012c78081ff0
tory <tadams@thegameclash.com>
2014-07-25 21:23:04 -0700

    imports graphs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 var cmds = {};
 var cmdString = process.argv.join(" ");
 var fs = require("fs");
-
+var async = require("async");
 
 //double check to make sure the cmdstring has the parameters needed.
 
@@ -46,36 +46,80 @@ cmds["devApp"] = require("orchestrate")(cmds.devkey);
 //short variable for live app data;
 var data = cmds["liveAppData"];
 
-//loop over array
 
-for(var i = 0; i < data.length; i++){
 
-  //make sure array el is not empty string
-  if(data[i] != ""){
-    var obj = JSON.parse(data[i])
-    if(obj.kind == "item"){
-
-      //put the object in the dev app
-
-      cmds["devApp"].put(obj.path.collection, obj.path.key, obj.value)
-
-      //if err throw
-
-      .fail(function (err){
-        if(err){
-          throw new Error(err);
-        }
+async.series([
+    function (callback){
+      loopOverArray('item', data, 'one', function (){
+        callback(null, 'one');
       });
+    },
+    function (callback){
+      loopOverArray('relationship', data, 'one', function (){
+        callback(null, 'two');
+      });
+    }
+]);
+
+/**
+  * loops over the array pulled from exported file
+  * @param {string} kind (the kind of object it is)
+  * @param {array} array (array of data read from file)
+  * @param {string} seriesPos (which index of async.series function it is)
+**/
+
+function loopOverArray (kind, array, seriesPos, callback){
+
+  var finishCount = 0;
+  var runCount = 0;
+
+  array.forEach(function (obj){
+    if(obj != ""){
+      obj = JSON.parse(obj);
+      if(obj.kind == kind){
+        finishCount++;
+      }
+    }
+  });
+
+  for(var i = 0; i < array.length; i++){
+    if(array[i] != ""){
+      var obj = JSON.parse(array[i]);
+      if(obj.kind == kind){
+        db = fetchNextPiece(obj, cmds["devApp"]);
+        db
+        .then(function (result){
+          runCount++;
+          if(runCount == finishCount){
+            callback();
+          }
+        })
+        .fail(function (err){
+          throw new Error(err);
+        });
+      }
     }
   }
 }
 
+/**
+  * builds majority of the orchestrate call
+  * @param {object} obj (single object from file)
+  * @param {object} db (orchestrate module db object)
+**/
 
+function fetchNextPiece(obj, db){
+  if(obj.kind == "item"){
+    return db.put(obj.path.collection, obj.path.key, obj.value);
+  } else if (obj.kind == "relationship"){
+    return db.newGraphBuilder().create().from(obj.source.collection, obj.source.key).related(obj.relation).to(obj.destination.collection, obj.destination.key)
+  }
+}
 
 /**
   * loads file
   * @param {string} pathToFile (exported data from live app on orchestrate)
-**/ 
+**/
 
 function orchestrateObject(pathToFile) {
   return fs.readFileSync(pathToFile, {encoding: "utf8"}).split("\n");
